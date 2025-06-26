cec626f772e6644d81e9498a4bd3424e99070993
VanillaNahida <nahida1027@126.com>
2025-06-26 21:32:14 +0800

    first commit

---
+++ 一键启动小智服务端.bat
@@ -0,0 +1,12 @@
+@echo off
+set "BATCH_DIR=%~dp0"
+set "PYTHON_PATH=%BATCH_DIR%runtime\python.exe"
+set "PATH=%BATCH_DIR%runtime\ffmpeg;%PATH%"
+set "PATH=%BATCH_DIR%runtime\mysql\bin;%PATH%"
+set "PATH=%BATCH_DIR%runtime\Redis;%PATH%"
+set "PATH=%BATCH_DIR%runtime\nodejs;%PATH%"
+set "PATH=%BATCH_DIR%runtime\jdk-21\bin;%PATH%"
+cd /d "%BATCH_DIR%src\main\xiaozhi-server"
+title Ð¡ÖÇAI·þÎñ¶Ë
+"%PYTHON_PATH%" app.py
+pause
